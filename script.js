210329fa419f447d20eb92b7cbbea75a95d133e0
Shelly <shellyniazov11@gmail.com>
2020-03-12 10:31:02 +0200

    Eror

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-let More;
-let first = 5;
-let second = 8;
+var row = parseInt(prompt('Please enter a number'));
+var column = parseInt(prompt('Please enter a number'));
 
 
-More = multiplyNum(first, second);
-
-function multiplyNum(Num1, Num2) {
-    document.write('<table>');
+createTable(row, column);
 
 
-    document.write('<tr>')
-    for (i = 0; i <= Num1; i++) {
-        document.write('<td>' + i + '</td>')
-    }
+function createTable(row = 1, column = 1) {
+
+    if (row > 0 && column > 0) {
+        let tableHeader = '';
 
 
+        tableHeader += '<tr>'
+        for (i = 0; i <= row; i++) {
+            tableHeader += '<td>' + i + '</td>'
+        }
+        tableHeader += '</tr>'
 
+        let tableBody = '';
 
-    for (r = 1; r <= Num2; r++) {
-        document.write('<tr>')
-        document.write('<td>' + r + '</td>')
+        for (r = 1; r <= column; r++) {
 
-        for (i = 1; i <= Num1; i++) {
-            document.write('<td>' + i * r + '</td>')
+            tableBody += '<tr>'
+            tableBody += '<td>' + r + '</td>'
+
+            for (i = 1; i <= row; i++) {
+                tableBody += '<td>' + i * r + '</td>'
+            }
+
+            tableBody += '</tr>'
+        }
+        document.write(`<table border> ${tableHeader,tableBody} </table>`);
+    } else {
+        if (row <= 0 || column <= 0) {
+            let row = prompt(`Try again - Please enter a new number`);
+            let column = prompt(`Please enter one more number`);
+            createTable(row, column);
         }
 
-        document.write('</tr>')
+
     }
-    document.write('</table>');
 }
